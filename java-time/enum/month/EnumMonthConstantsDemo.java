8d8c8ced2457ed87b9b7230cec1e9520efa68b6f
Rohit Agarwal <programmingwithrohit@gmail.com>
2018-01-05 20:59:21 +0530

    Update EnumMonthConstantsDemo.java

--- java-time/enum/month/EnumMonthConstantsDemo.java
+++ java-time/enum/month/EnumMonthConstantsDemo.java
@@ -12,18 +12,18 @@ public class EnumMonthConstantsDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Month names : ");
-		System.out.println(Month.JANUARY);
-		System.out.println(Month.FEBRUARY);
-		System.out.println(Month.MARCH);
-		System.out.println(Month.APRIL);
-		System.out.println(Month.MAY);
-		System.out.println(Month.JUNE);
-		System.out.println(Month.JULY);
-		System.out.println(Month.AUGUST);
-		System.out.println(Month.SEPTEMBER);
-		System.out.println(Month.OCTOBER);
-		System.out.println(Month.NOVEMBER);
-		System.out.println(Month.DECEMBER);
+		System.out.println(Month.JANUARY);//JANUARY
+		System.out.println(Month.FEBRUARY);//FEBRUARY
+		System.out.println(Month.MARCH);//MARCH
+		System.out.println(Month.APRIL);//APRIL
+		System.out.println(Month.MAY);//MAY
+		System.out.println(Month.JUNE);//JUNE
+		System.out.println(Month.JULY);//JULY
+		System.out.println(Month.AUGUST);//AUGUST
+		System.out.println(Month.SEPTEMBER);//SEPTEMBER
+		System.out.println(Month.OCTOBER);//OCTOBER
+		System.out.println(Month.NOVEMBER);//NOVEMBER
+		System.out.println(Month.DECEMBER);//DECEMBER
 
 	}
 
