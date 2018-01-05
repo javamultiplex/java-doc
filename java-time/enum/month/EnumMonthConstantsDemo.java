68fe63d0760afbccb0b67f6422f781da377e0236
Rohit Agarwal <programmingwithrohit@gmail.com>
2018-01-05 20:57:30 +0530

    Create EnumMonthConstantsDemo.java

---
+++ java-time/enum/month/EnumMonthConstantsDemo.java
@@ -0,0 +1,30 @@
+package com.javamultiplex.enums;
+
+import java.time.Month;
+
+/**
+ * 
+ * @author Rohit Agarwal
+ * @version 1.8
+ *
+ */
+public class EnumMonthConstantsDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Month names : ");
+		System.out.println(Month.JANUARY);
+		System.out.println(Month.FEBRUARY);
+		System.out.println(Month.MARCH);
+		System.out.println(Month.APRIL);
+		System.out.println(Month.MAY);
+		System.out.println(Month.JUNE);
+		System.out.println(Month.JULY);
+		System.out.println(Month.AUGUST);
+		System.out.println(Month.SEPTEMBER);
+		System.out.println(Month.OCTOBER);
+		System.out.println(Month.NOVEMBER);
+		System.out.println(Month.DECEMBER);
+
+	}
+
+}
