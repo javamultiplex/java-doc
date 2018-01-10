c8d480662f2a5296150640489e347aaebd913141
Rohit Agarwal <programmingwithrohit@gmail.com>
2018-01-10 20:52:27 +0530

    Create EnumDayOfWeekConstantsDemo.java

---
+++ java-time/enum/day-of-week/EnumDayOfWeekConstantsDemo.java
@@ -0,0 +1,20 @@
+package com.javamultiplex.enums;
+
+import java.time.DayOfWeek;
+
+public class EnumDayOfWeekConstantsDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Days of week.");
+		System.out.println(DayOfWeek.MONDAY);//MONDAY
+		System.out.println(DayOfWeek.TUESDAY);//TUESDAY
+		System.out.println(DayOfWeek.WEDNESDAY);//WEDNESDAY
+		System.out.println(DayOfWeek.THURSDAY);//THURSDAY
+		System.out.println(DayOfWeek.FRIDAY);//FRIDAY
+		System.out.println(DayOfWeek.SATURDAY);//SATURDAY
+		System.out.println(DayOfWeek.SUNDAY);//SUNDAY
+		
+	}
+	
+}
