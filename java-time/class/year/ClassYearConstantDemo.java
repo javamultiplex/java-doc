51922be3cd96b23fefce44614363a78df857ef64
Rohit Agarwal <programmingwithrohit@gmail.com>
2018-01-16 17:43:48 +0530

    Create ClassYearConstantDemo.java

---
+++ java-time/class/year/ClassYearConstantDemo.java
@@ -0,0 +1,16 @@
+package com.javamultiplex.classs;
+
+import java.time.Year;
+
+public class ClassYearConstantDemo {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Minimum value of Year : "+Year.MIN_VALUE);
+		System.out.println("Maximum value of Year : "+Year.MAX_VALUE);
+		
+	}
+	
+}
